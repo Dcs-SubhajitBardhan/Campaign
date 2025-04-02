e3abfa63f1d07be0ac122123d48df1f0dff742da
unknown <subhajit.bardhan@divergentcs.in>
2025-04-02 17:33:57 +0530

    UI DONE

--- drawer.js
+++ drawer.js
@@ -2,3 +2,7 @@ function toggleDrawer(){
     const drawer  = document.getElementById('drawer');
     drawer.style.width =drawer.style.width === '250px' ? '0' : '250px';
 }
+function toggleSubMenu(id) {
+    const submenu = document.getElementById(id);
+    submenu.style.display = submenu.style.display === 'block' ? 'none' : 'block';
+}
